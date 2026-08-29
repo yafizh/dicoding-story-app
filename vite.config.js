65e3aa620ca10591d8540f7cc71a9c4d0cb0b584
yafizh <a.chairil.yafizh@gmail.com>
2026-08-29 16:38:58 +0700

    add env dir

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src', 'public'),
+  envDir: resolve(__dirname),
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
